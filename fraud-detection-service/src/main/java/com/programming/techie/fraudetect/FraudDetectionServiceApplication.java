aa3cdc78fea1e38d11fa1df6e7d054565ee943f9
sajad <sajad.sdp3@gmail.com>
2023-10-10 20:01:05 +0330

    final

--- fraud-detection-service/src/main/java/com/programming/techie/fraudetect/FraudDetectionServiceApplication.java
+++ fraud-detection-service/src/main/java/com/programming/techie/fraudetect/FraudDetectionServiceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FraudDetectionServiceApplication {
+public class  FraudDetectionServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FraudDetectionServiceApplication.class, args);
